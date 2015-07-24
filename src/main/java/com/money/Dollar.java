d0b9753eda89e9d742e6d0bc4a3fd0be740b9f16
billy thach <bthach@mirakl.com>
2015-07-25 01:36:02 +0200

    Refactoring Dollar equals

--- src/main/java/com/money/Dollar.java
+++ src/main/java/com/money/Dollar.java
@@ -13,8 +13,8 @@ public class Dollar extends Money {
 
     @Override
     public boolean equals(Object o) {
-        Dollar dollar= (Dollar) o;
-        return amount == dollar.amount;
+        Money money = (Money) o;
+        return amount == money.amount;
     }
 
 }
