b19faf3b17d4077950d4761615c327738d69eeac
billy thach <bthach@mirakl.com>
2015-07-25 00:54:59 +0200

    Rewrite multiplication test

--- src/test/java/com/money/DollarTest.java
+++ src/test/java/com/money/DollarTest.java
@@ -10,9 +10,9 @@ public class DollarTest {
     public void testMultiplication() {
         Dollar five= new Dollar(5);
         Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
+        assertThat(product).isEqualTo(new Dollar(10));
         product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertThat(product).isEqualTo(new Dollar(15));
     }
 
     public void testEquality() {
