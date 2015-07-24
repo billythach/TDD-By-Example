5160adf9c7ff4a9853ed72cdc5601b2baf18f1a6
billy thach <bthach@mirakl.com>
2015-07-25 01:37:13 +0200

    Move Dollar equals to Money

--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -4,4 +4,10 @@ public class Money {
 
     protected int amount;
 
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
 }
