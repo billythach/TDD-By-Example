cad50846242a78ec15c8bb40a14d585e6fa5afce
billy thach <bthach@mirakl.com>
2015-07-25 23:50:03 +0200

    Delete duplication on  Dollar/Franc constructor

--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -5,6 +5,11 @@ public abstract class Money {
     protected int amount;
     protected String currency;
 
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
