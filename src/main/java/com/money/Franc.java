52a68966f30af0d70d664bba6378a74b292fa432
billy thach <bthach@mirakl.com>
2015-07-25 23:50:03 +0200

    Delete duplication on  Dollar/Franc constructor

--- src/main/java/com/money/Franc.java
+++ src/main/java/com/money/Franc.java
@@ -3,8 +3,7 @@ package com.money;
 public class Franc extends Money {
 
     public Franc(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
 
     public Money times(int i) {
