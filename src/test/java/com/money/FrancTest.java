ca73605aae3336143b78e141bea71bdc24ccc31b
billy thach <bthach@mirakl.com>
2015-07-25 22:41:09 +0200

    Create factory method Money.franc

--- src/test/java/com/money/FrancTest.java
+++ src/test/java/com/money/FrancTest.java
@@ -8,9 +8,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FrancTest {
 
     public void testFrancMultiplication() {
-        Franc five= new Franc(5);
-        assertThat(new Franc(10)).isEqualTo(five.times(2));
-        assertThat(new Franc(15)).isEqualTo(five.times(3));
+        Money five= Money.franc(5);
+        assertThat(Money.franc(10)).isEqualTo(five.times(2));
+        assertThat(Money.franc(15)).isEqualTo(five.times(3));
     }
 
 }
