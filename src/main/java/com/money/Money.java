971a98479304ef5ddcf6e15f52ef9b0f20c91f3a
billy thach <bthach@mirakl.com>
2015-07-26 01:47:19 +0200

    Delete Franc/Dollar class

--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -11,11 +11,11 @@ public class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     String currency() {
