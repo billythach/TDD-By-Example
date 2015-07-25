9dd0d58912f8cd15883ae0715abd3cab21de2c7e
billy thach <bthach@mirakl.com>
2015-07-26 01:48:05 +0200

    Delete test equality duplication

--- src/test/java/com/money/MoneyTest.java
+++ src/test/java/com/money/MoneyTest.java
@@ -10,8 +10,6 @@ public class MoneyTest {
     public void testEquality() {
         assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
         assertThat(Money.dollar(6)).isNotEqualTo(Money.dollar(5));
-        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
-        assertThat(Money.franc(6)).isNotEqualTo(Money.franc(5));
         assertThat(Money.franc(5)).isNotEqualTo(Money.dollar(5));
     }
 
