7d3a7e3fd6ecc5f0388d408bb654115eb9020882
billy thach <bthach@mirakl.com>
2015-07-27 12:11:25 +0200

    Refactoring Bank.rates with add of Pair<from,to> rate

--- src/main/java/com/money/Bank.java
+++ src/main/java/com/money/Bank.java
@@ -1,16 +1,24 @@
 package com.money;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     public Money reduce(Expression expression, String to) {
         return expression.reduce(this, to);
     }
 
-    public void addRate(String from, String to, int i) {
-
+    public void addRate(String from, String to, int rate) {
+        this.rates.put(new Pair(from, to), rate);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if(from.equals(to)) {
+            return 1;
+        }
+        Integer rate = (Integer) this.rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 }
