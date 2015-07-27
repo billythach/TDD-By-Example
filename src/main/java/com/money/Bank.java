6b4c33b7a8d56d37abdc30ac22fa968f030f6476
billy thach <bthach@mirakl.com>
2015-07-27 11:24:49 +0200

    Refactoring move reduce impl From Bank to Sum

--- src/main/java/com/money/Bank.java
+++ src/main/java/com/money/Bank.java
@@ -4,8 +4,7 @@ public class Bank {
 
     public Money reduce(Expression expression, String to) {
         Sum sum = (Sum) expression;
-        int amount = sum.addend.amount + sum.augend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 
 }
--- src/main/java/com/money/Sum.java
+++ src/main/java/com/money/Sum.java
@@ -8,4 +8,9 @@ public class Sum implements Expression {
         this.augend = augend;
         this.addend = addend;
     }
+
+    public Money reduce(String to) {
+        int amount = this.augend.amount + this.addend.amount;
+        return new Money(amount, to);
+    }
 }
