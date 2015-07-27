713b64ab6a11461df999578ab5e83b5828765867
billy thach <bthach@mirakl.com>
2015-07-27 11:57:09 +0200

    Refactoring Expression.reduce with Bank.rate

--- src/main/java/com/money/Sum.java
+++ src/main/java/com/money/Sum.java
@@ -9,7 +9,7 @@ public class Sum implements Expression {
         this.addend = addend;
     }
 
-    public Money reduce(String to) {
+    public Money reduce(Bank bank, String to) {
         int amount = this.augend.amount + this.addend.amount;
         return new Money(amount, to);
     }
