8df412283caf96b671c6c9e99ffbc23e9db7f86c
billy thach <bthach@mirakl.com>
2015-07-27 11:34:47 +0200

    Fix Bank.reduce money test with ugly impl

--- src/main/java/com/money/Bank.java
+++ src/main/java/com/money/Bank.java
@@ -3,6 +3,9 @@ package com.money;
 public class Bank {
 
     public Money reduce(Expression expression, String to) {
+        if(expression instanceof Money) {
+            return (Money) expression;
+        }
         Sum sum = (Sum) expression;
         return sum.reduce(to);
     }
