2d49c3640f5ea2bbe526662a3b5cda1ce1f7dba4
billy thach <bthach@mirakl.com>
2015-07-27 11:36:54 +0200

    Refactoring Bank.reduce

--- src/main/java/com/money/Bank.java
+++ src/main/java/com/money/Bank.java
@@ -3,11 +3,7 @@ package com.money;
 public class Bank {
 
     public Money reduce(Expression expression, String to) {
-        if(expression instanceof Money) {
-            return (Money) expression;
-        }
-        Sum sum = (Sum) expression;
-        return sum.reduce(to);
+        return expression.reduce(to);
     }
 
 }
