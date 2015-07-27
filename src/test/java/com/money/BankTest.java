b4793b1e410e9ab53c7a70d66c919ba2467be6d9
billy thach <bthach@mirakl.com>
2015-07-27 11:16:51 +0200

    Add Bank.reduce test

---
+++ src/test/java/com/money/BankTest.java
@@ -0,0 +1,17 @@
+package com.money;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Test
+public class BankTest {
+
+    public void testReduceSum() {
+        Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank= new Bank();
+        Money result= bank.reduce(sum, "USD");
+        assertThat(result).isEqualTo(Money.dollar(7));
+    }
+
+}
