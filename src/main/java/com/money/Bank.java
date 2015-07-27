5016e78503c7bcd589864b94ffd3cc05a218da19
billy thach <bthach@mirakl.com>
2015-07-27 11:57:09 +0200

    Refactoring Expression.reduce with Bank.rate

--- src/main/java/com/money/Bank.java
+++ src/main/java/com/money/Bank.java
@@ -3,10 +3,14 @@ package com.money;
 public class Bank {
 
     public Money reduce(Expression expression, String to) {
-        return expression.reduce(to);
+        return expression.reduce(this, to);
     }
 
     public void addRate(String from, String to, int i) {
 
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
--- src/main/java/com/money/Money.java
+++ src/main/java/com/money/Money.java
@@ -45,10 +45,8 @@ public class Money implements Expression {
     }
 
     @Override
-    public Money reduce(String to) {
-        int rate = (currency.equals("CHF") && to.equals("USD"))
-                ?2
-                : 1;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
         return new Money(amount / rate, to);
     }
 }
